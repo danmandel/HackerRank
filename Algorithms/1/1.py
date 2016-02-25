52f8af49d7f8d0fa3c22f031f35ef1273552e931
danmandel <danmandel@users.noreply.github.com>
2016-02-24 20:04:31 -0500

    Create 1.py

---
+++ Algorithms/1/1.py
@@ -0,0 +1,8 @@
+# Returns sum of two integers passed as input parameters.
+def solveMeFirst(a,b):
+  return num1+num2
+
+num1 = int(input())
+num2 = int(input())
+res = solveMeFirst(num1,num2)
+print(res)
