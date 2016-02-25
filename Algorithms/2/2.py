2574992cbcf8b298ed414d6e6bf6fa0f21787240
danmandel <danmandel@users.noreply.github.com>
2016-02-24 21:29:18 -0500

    Create 2.py

---
+++ Algorithms/2/2.py
@@ -0,0 +1,10 @@
+# Finds the sum of elements in the array
+import sys
+
+n = int(input().strip())
+arr = [int(arr_temp) for arr_temp in input().strip().split(' ')]
+
+result = 0
+for i in arr:
+    result += i
+print (result)
